c1e296049183cb399746aec56fca4b64acfc36bf
irailtonreis <irailton.oliveira91@gmail.com>
2023-07-06 13:22:46 -0300

    feat: Adiciona botões anterior e próximo

--- src/Slide.ts
+++ src/Slide.ts
@@ -17,15 +17,39 @@ export default class Slide {
     this.time = time;
     this.index = 0;
     this.slide = this.slides[this.index]
-    this.show(0)
+    this.init()
   }
-  hide(el: Element){
+  hide(el: Element) {
     el.classList.remove("active")
   }
-  show(index: number){
-    this.index = index; 
+  show(index: number) {
+    this.index = index;
     this.slide = this.slides[this.index]
-    this.slides.forEach((el)=>this.hide(el))
+    this.slides.forEach((el) => this.hide(el))
     this.slide.classList.add('active')
+  }
+  prev() {
+    const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1;
+    this.show(prev)
+  }
+
+  next() {
+    const next = this.index + 1 < this.slides.length ? this.index + 1 : 0;
+    this.show(next)
+  }
+
+  private addControls() {
+    const prevButton = document.createElement('button')
+    const nextButton = document.createElement('button')
+    this.controls.appendChild(prevButton)
+    this.controls.appendChild(nextButton)
+    prevButton.innerText = "Slide Anterior"
+    nextButton.innerText = "Próximo Slide"
+    prevButton.addEventListener('pointerup', () => this.prev())
+    nextButton.addEventListener('pointerup', () => this.next())
+  }
+  private init() {
+    this.addControls()
+    this.show(0)
   }
 }
