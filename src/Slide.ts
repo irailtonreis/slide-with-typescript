db832cd7c4bc46ffbd493b03aedcfb7a58bac22a
irailtonreis <irailton.oliveira91@gmail.com>
2023-07-09 16:11:12 -0300

    feat: tempo de video slide conforme video e slide ativo local storage

--- src/Slide.ts
+++ src/Slide.ts
@@ -24,48 +24,69 @@ export default class Slide {
     this.paused = false
     this.timeout = null;
     this.pausedTimeout = null;
-    this.index = 0;
+    this.index = localStorage.getItem('activeSlide') ? Number(localStorage.getItem('activeSlide')) : 0;
     this.slide = this.slides[this.index];
 
     this.init();
   }
   hide(el: Element) {
     el.classList.remove("active");
+    if (el instanceof HTMLVideoElement) {
+      el.currentTime = 0;
+      el.pause()
+    }
   }
   show(index: number) {
     this.index = index;
     this.slide = this.slides[this.index];
+    localStorage.setItem('activeSlide', String(this.index))
     this.slides.forEach((el) => this.hide(el));
     this.slide.classList.add("active");
-    this.auto(this.time);
+    if (this.slide instanceof HTMLVideoElement) {
+      this.autoVideo(this.slide)
+    } else {
+      this.auto(this.time);
+    }
+  }
+  autoVideo(video: HTMLVideoElement) {
+    video.muted = true
+    video.play()
+    let firstPlay = true
+    video.addEventListener('playing', () => {
+      if (firstPlay) this.auto(video.duration * 1000)
+    })
+
   }
   auto(time: number) {
     this.timeout?.clear();
     this.timeout = new Timeout(() => this.next(), time);
   }
   prev() {
-    if(this.paused) return
+    if (this.paused) return
     const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1;
     this.show(prev);
   }
   next() {
-    if(this.paused) return
+    if (this.paused) return
     const next = this.index + 1 < this.slides.length ? this.index + 1 : 0;
     this.show(next);
   }
-  pause(){
+  pause() {
 
-    this.pausedTimeout = new Timeout(()=>{
+    this.pausedTimeout = new Timeout(() => {
       this.paused = true
       this.timeout?.pause()
+      if (this.slide instanceof HTMLVideoElement) this.slide.pause()
     }, 300)
   }
 
-  contiue(){
+  contiue() {
     this.pausedTimeout?.clear()
-    if(this.paused){
+    if (this.paused) {
       this.paused = false
       this.timeout?.continue()
+      if (this.slide instanceof HTMLVideoElement) this.slide.play()
+
     }
   }
   private addControls() {
@@ -76,8 +97,8 @@ export default class Slide {
     this.controls.appendChild(prevButton);
     this.controls.appendChild(nextButton);
 
-    this.controls.addEventListener("pointerdown", ()=>this.pause())
-    this.controls.addEventListener("pointerup", ()=>this.contiue())
+    this.controls.addEventListener("pointerdown", () => this.pause())
+    this.controls.addEventListener("pointerup", () => this.contiue())
     prevButton.addEventListener("pointerup", () => this.prev());
     nextButton.addEventListener("pointerup", () => this.next());
   }
